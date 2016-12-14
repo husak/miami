906907e571ca62ebeb91dab2469ba935b205f8b2
Martin Husák <husakm@ics.muni.cz>
2016-12-14 22:31:23 +0100

    export to IDEA

--- miami.sh
+++ miami.sh
@@ -2,11 +2,14 @@
 
 DIR=/opt/cowrie/dl
 LOG=/var/log/miami.log
+WARDEN_DIR=""
 
-# continuously checking new files
+# checking new files in a directory
 inotifywait -m -q -e create --format '%w%f' $DIR | while read FILE
   do
     DATE=`date +"%Y-%m-%d %H:%M:%S"`
+    declare -a IPLIST
+    COUNT=0
     # check if file is text or binary
     if file -i $FILE | grep -q "text"; then
       echo "$DATE new malware stored in text file $FILE" >> $LOG
@@ -14,6 +17,8 @@ inotifywait -m -q -e create --format '%w%f' $DIR | while read FILE
       grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" $FILE | while read IP
         do
           echo "$DATE IP $IP found in file $FILE" >> $LOG
+          IPLIST[$COUNT]=$IP
+          COUNT=$((COUNT+1))
         done
     else
       echo "$DATE new malware stored in binary file $FILE" >> $LOG
@@ -21,6 +26,32 @@ inotifywait -m -q -e create --format '%w%f' $DIR | while read FILE
       strings $FILE | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | while read IP
         do
           echo "$DATE IP $IP found in file $FILE" >> $LOG
+          IPLIST[$COUNT]=$IP
+          COUNT=$((COUNT+1))
         done
     fi
+    # prepare IDEA event
+    if $COUNT > 0
+      IDEA='{
+"Format": "IDEA0",
+"DetectTime": "'$DATE'",
+"Category": ["Malware.Virus", "Intrusion.Botnet"],
+"Source": [{
+    "IP4": ['
+      for i in `seq 0 $COUNT`;
+      do
+        IDEA+=$IPLIST[$i]
+      done
+      IDEA+=']
+  }],
+"Node": [{
+    "Name": ["miami"],
+    "Type": ["Honeypot"],
+    "SW": ["MIAMI"]
+  }]
+}'
+      # write IDEA to file
+      echo $IDEA > $WARDEN_DIR/miami.idea
+    fi
+    unset IPLIST
   done
